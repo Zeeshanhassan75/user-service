14a073febba21e002374c2efc2fa59ce4438e8ee
Zeeshanhassan75 <hassan.zeeshan7767@gmail.com>
2025-08-10 21:29:32 +0530

    Resiliance4J CircuitBreaker and RateLimiter apply

--- src/main/java/com/microservice/userservice/controller/UserServiceControlller.java
+++ src/main/java/com/microservice/userservice/controller/UserServiceControlller.java
@@ -1,7 +1,6 @@
 package com.microservice.userservice.controller;
 
 import java.util.List;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -14,6 +13,10 @@ import org.springframework.web.bind.annotation.RestController;
 
 import com.microservice.userservice.Interface.UserServiceInterface;
 import com.microservice.userservice.entity.User;
+
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
+import io.github.resilience4j.ratelimiter.annotation.RateLimiter;
+
 
 @RestController
 @RequestMapping("/userService")
@@ -29,10 +32,28 @@ public class UserServiceControlller {
 		return ResponseEntity.status(HttpStatus.CREATED).body(user1);
 	}
 	
+	int count = 1;
 	@GetMapping("/{UserId}")
+	@CircuitBreaker(name="RatingHotelServiceBreaker" , fallbackMethod = "ratingHotelServiceFallback")
+	@RateLimiter(name="RatingHotelService" , fallbackMethod = "ratingHotelServiceFallback")
 	ResponseEntity<User> GetUserById(@PathVariable String UserId){
+		System.out.println("Rate Count : "+count);
+		count++;
 		User user1 = userServiceInterface.getSingleUser(UserId);
 		return ResponseEntity.ok(user1);
+	}
+	
+	ResponseEntity<User> ratingHotelServiceFallback(String userId , Exception ex){
+		System.out.println("RATING AND HOTEL SERVICE IS DOWN");
+		System.out.println("");
+		new User();
+		User user = User.builder()
+					.email("dummy@gmail.com")
+					.userName("Dummy USER")
+					.userId("123")
+					.build();
+		
+		return new ResponseEntity<>(user, HttpStatus.OK);
 	}
 	
 	@GetMapping("/getAllUsers")
